c29bbeb0e9ea60cd1acacf85ca80c014caea5c83
Taylor Fausak <taylor@fausak.me>
2012-09-01 13:27:16 -0500

    Fix #1; prevent skipping delay with inactive windows.
    This doesn't reset the delay as suggested in issue #1. Instead it polls
    every second and only counts that second if the window is active.

--- delay.safariextension/start.js
+++ delay.safariextension/start.js
@@ -1,23 +1,37 @@
 (function () {
     "use strict";
-    var settings, visibility;
+    var active, settings, visibility, tick, elapsed, intervalID;
 
-    if (window !== window.top) {
-        return;
+    if (window === window.top) {
+        safari.self.tab.dispatchMessage('getSettings');
+
+        active = true;
+        window.onblur = function (event) {
+            active = false;
+        };
+        window.onfocus = function (event) {
+            active = true;
+        };
     }
-
-    safari.self.tab.dispatchMessage('getSettings');
 
     safari.self.addEventListener('message', function (event) {
         if (event.name === 'settings') {
             settings = event.message;
-
             if (settings.blacklist.indexOf(window.location.hostname) !== -1) {
                 visibility = document.documentElement.style.visibility;
                 document.documentElement.style.visibility = 'hidden';
-                window.setTimeout(function () {
-                    document.documentElement.style.visibility = visibility;
-                }, settings.delay);
+
+                tick = 1000;
+                elapsed = 0;
+                intervalID = window.setInterval(function () {
+                    if (active) {
+                        elapsed += tick;
+                    }
+                    if (elapsed >= settings.delay) {
+                        document.documentElement.style.visibility = visibility;
+                        window.clearInterval(intervalID);
+                    }
+                }, tick);
             }
         }
     }, false);
