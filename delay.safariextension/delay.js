fbee630ada3f3e1ed71d43a9680ac983e51716b4
Taylor Fausak <taylor@fausak.me>
2012-08-31 19:48:31 -0500

    Implement simple delay script.
    It adds an id of "delay" to the root element (<html>) for the duration
    of the delay.

--- delay.safariextension/delay.js
+++ delay.safariextension/delay.js
@@ -1,3 +1,13 @@
 (function () {
     "use strict";
+
+    safari.application.addEventListener('message', function (event) {
+        if (event.name === 'getSettings') {
+            event.target.page.dispatchMessage('settings', {
+                'delay': safari.extension.settings.delay,
+                'jitter': safari.extension.settings.jitter,
+                'blacklist': safari.extension.settings.blacklist.split(/\s+/).filter(function (hostname) { return hostname.toLowerCase(); }),
+            });
+        }
+    }, false);
 }());
--- delay.safariextension/start.js
+++ delay.safariextension/start.js
@@ -1,3 +1,24 @@
 (function () {
     "use strict";
+    var settings, id;
+
+    if (window !== window.top) {
+        return;
+    }
+
+    safari.self.tab.dispatchMessage('getSettings');
+
+    safari.self.addEventListener('message', function (event) {
+        if (event.name === 'settings') {
+            settings = event.message;
+
+            if (settings.blacklist.indexOf(window.location.hostname) !== -1) {
+                id = document.firstChild.id || '';
+                document.firstChild.id += ' delay';
+                window.setTimeout(function () {
+                    document.firstChild.id = id;
+                }, 1000 * (settings.delay - settings.jitter + (Math.random() * 2 * settings.jitter)));
+            }
+        }
+    }, false);
 }());
