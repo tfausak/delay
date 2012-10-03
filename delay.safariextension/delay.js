91eed12993627b899ae81f528df7261cd6e191bb
Taylor Fausak <taylor@fausak.me>
2012-10-03 16:58:57 -0500

    Fetch whitelist from settings.

--- delay.safariextension/delay.js
+++ delay.safariextension/delay.js
@@ -1,12 +1,13 @@
 (function () {
     "use strict";
-    var delay, jitter, blacklist;
+    var delay, jitter, blacklist, whitelist;
 
     safari.application.addEventListener('message', function (event) {
         if (event.name === 'getSettings') {
             delay = safari.extension.settings.delay;
             jitter = safari.extension.settings.jitter;
             blacklist = safari.extension.settings.blacklist;
+            whitelist = safari.extension.settings.whitelist;
 
             if (jitter) {
                 delay -= jitter;
@@ -18,10 +19,15 @@
                 return hostname.toLowerCase();
             });
 
+            whitelist = whitelist.split(/\s+/).filter(function (hostname) {
+                return hostname.toLowerCase();
+            });
+
             event.target.page.dispatchMessage('settings', {
                 'blacklist': blacklist,
                 'delay': delay,
-                'timer': safari.extension.settings.timer
+                'timer': safari.extension.settings.timer,
+                'whitelist': whitelist,
             });
         }
     }, false);
