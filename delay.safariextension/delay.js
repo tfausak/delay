4125ee0b1649daeb2353393241be17b37f464a19
Taylor Fausak <taylor@fausak.me>
2012-10-03 19:13:04 -0500

    Use two spaces for indentation instead of four.

--- delay.safariextension/delay.js
+++ delay.safariextension/delay.js
@@ -1,45 +1,45 @@
 (function () {
-    "use strict";
-    var delay, jitter, timer, list, blacklist, whitelist;
+  "use strict";
+  var delay, jitter, timer, list, blacklist, whitelist;
 
-    safari.application.addEventListener('message', function (event) {
-        if (event.name === 'getSettings') {
-            delay = safari.extension.settings.delay;
-            jitter = safari.extension.settings.jitter;
-            timer = safari.extension.settings.timer;
-            list = safari.extension.settings.list;
-            blacklist = safari.extension.settings.blacklist;
-            whitelist = safari.extension.settings.whitelist;
+  safari.application.addEventListener('message', function (event) {
+    if (event.name === 'getSettings') {
+      delay = safari.extension.settings.delay;
+      jitter = safari.extension.settings.jitter;
+      timer = safari.extension.settings.timer;
+      list = safari.extension.settings.list;
+      blacklist = safari.extension.settings.blacklist;
+      whitelist = safari.extension.settings.whitelist;
 
-            delay = parseInt(delay, 10);
-            if (isNaN(delay) || delay < 0) {
-                delay = 0;
-            }
-            jitter = parseInt(jitter, 10);
-            if (isNaN(jitter) || jitter < 0) {
-                jitter = 0;
-            }
-            if (jitter) {
-                delay -= jitter;
-                delay += 2 * jitter * Math.random();
-            }
-            delay *= 1000;
+      delay = parseInt(delay, 10);
+      if (isNaN(delay) || delay < 0) {
+        delay = 0;
+      }
+      jitter = parseInt(jitter, 10);
+      if (isNaN(jitter) || jitter < 0) {
+        jitter = 0;
+      }
+      if (jitter) {
+        delay -= jitter;
+        delay += 2 * jitter * Math.random();
+      }
+      delay *= 1000;
 
-            blacklist = blacklist.split(/\s+/).filter(function (hostname) {
-                return hostname.toLowerCase();
-            });
+      blacklist = blacklist.split(/\s+/).filter(function (hostname) {
+        return hostname.toLowerCase();
+      });
 
-            whitelist = whitelist.split(/\s+/).filter(function (hostname) {
-                return hostname.toLowerCase();
-            });
+      whitelist = whitelist.split(/\s+/).filter(function (hostname) {
+        return hostname.toLowerCase();
+      });
 
-            event.target.page.dispatchMessage('settings', {
-                'blacklist': blacklist,
-                'delay': delay,
-                'list': list,
-                'timer': timer,
-                'whitelist': whitelist,
-            });
-        }
-    }, false);
+      event.target.page.dispatchMessage('settings', {
+        'blacklist': blacklist,
+        'delay': delay,
+        'list': list,
+        'timer': timer,
+        'whitelist': whitelist,
+      });
+    }
+  }, false);
 }());
--- delay.safariextension/start.js
+++ delay.safariextension/start.js
@@ -1,51 +1,51 @@
 (function () {
-    "use strict";
-    var active = true, block, elapsed = 0, intervalID, settings, tick = 1000;
+  "use strict";
+  var active = true, block, elapsed = 0, intervalID, settings, tick = 1000;
 
-    if (window === window.top) {
-        safari.self.tab.dispatchMessage('getSettings');
-        window.onblur = function () { active = false; };
-        window.onfocus = function () { active = true; };
-    }
+  if (window === window.top) {
+    safari.self.tab.dispatchMessage('getSettings');
+    window.onblur = function () { active = false; };
+    window.onfocus = function () { active = true; };
+  }
 
-    safari.self.addEventListener('message', function (event) {
-        if (event.name === 'settings') {
-            settings = event.message;
+  safari.self.addEventListener('message', function (event) {
+    if (event.name === 'settings') {
+      settings = event.message;
 
-            if (settings.list === 'whitelist') {
-                block = settings.whitelist.indexOf(window.location.hostname) === -1;
+      if (settings.list === 'whitelist') {
+        block = settings.whitelist.indexOf(window.location.hostname) === -1;
+      }
+      else {
+        block = settings.blacklist.indexOf(window.location.hostname) !== -1;
+      }
+
+      if (block) {
+        if (settings.timer) {
+          document.documentElement.setAttribute('delay',
+          Math.round(settings.delay / 1000));
+        }
+        else {
+          document.documentElement.setAttribute('delay', '');
+        }
+
+        intervalID = window.setInterval(function () {
+          if (active) {
+            elapsed += tick;
+
+            if (settings.timer) {
+              document.documentElement.setAttribute('delay',
+              Math.round((settings.delay - elapsed) / 1000));
             }
             else {
-                block = settings.blacklist.indexOf(window.location.hostname) !== -1;
+              document.documentElement.setAttribute('delay', '');
             }
-
-            if (block) {
-                if (settings.timer) {
-                    document.documentElement.setAttribute('delay',
-                        Math.round(settings.delay / 1000));
-                }
-                else {
-                    document.documentElement.setAttribute('delay', '');
-                }
-
-                intervalID = window.setInterval(function () {
-                    if (active) {
-                        elapsed += tick;
-
-                        if (settings.timer) {
-                            document.documentElement.setAttribute('delay',
-                                Math.round((settings.delay - elapsed) / 1000));
-                        }
-                        else {
-                            document.documentElement.setAttribute('delay', '');
-                        }
-                    }
-                    if (settings.delay - elapsed < 500) {
-                        document.documentElement.removeAttribute('delay');
-                        window.clearInterval(intervalID);
-                    }
-                }, tick);
-            }
-        }
-    }, false);
+          }
+          if (settings.delay - elapsed < 500) {
+            document.documentElement.removeAttribute('delay');
+            window.clearInterval(intervalID);
+          }
+        }, tick);
+      }
+    }
+  }, false);
 }());
