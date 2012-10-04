0d1d30fc7918a92012b76f691b51e98a7d5ea656
Taylor Fausak <taylor@fausak.me>
2012-10-03 20:07:37 -0500

    Rewrite JavaScript.
    Functionally, nothing has changed. I think this way is a lot cleaner
    and easier to understand.

--- delay.safariextension/delay.js
+++ delay.safariextension/delay.js
@@ -1,45 +1,39 @@
 (function () {
   "use strict";
-  var delay, jitter, timer, list, blacklist, whitelist;
 
   safari.application.addEventListener('message', function (event) {
-    if (event.name === 'getSettings') {
-      delay = safari.extension.settings.delay;
-      jitter = safari.extension.settings.jitter;
-      timer = safari.extension.settings.timer;
-      list = safari.extension.settings.list;
-      blacklist = safari.extension.settings.blacklist;
-      whitelist = safari.extension.settings.whitelist;
-
-      delay = parseInt(delay, 10);
-      if (isNaN(delay) || delay < 0) {
-        delay = 0;
-      }
-      jitter = parseInt(jitter, 10);
-      if (isNaN(jitter) || jitter < 0) {
-        jitter = 0;
-      }
-      if (jitter) {
-        delay -= jitter;
-        delay += 2 * jitter * Math.random();
-      }
-      delay *= 1000;
-
-      blacklist = blacklist.split(/\s+/).filter(function (hostname) {
-        return hostname.toLowerCase();
-      });
-
-      whitelist = whitelist.split(/\s+/).filter(function (hostname) {
-        return hostname.toLowerCase();
-      });
-
-      event.target.page.dispatchMessage('settings', {
-        'blacklist': blacklist,
-        'delay': delay,
-        'list': list,
-        'timer': timer,
-        'whitelist': whitelist,
-      });
+    if (event.name === 'requestSettings') {
+      return requestSettings(event);
     }
   }, false);
+
+  function requestSettings (event) {
+    var settings = safari.extension.settings,
+      jitter, delay, blacklist, whitelist;
+
+    jitter = parseInt(settings.jitter, 10);
+    if (isNaN(jitter) || jitter < 0) {
+      jitter = 0;
+    }
+
+    delay = parseInt(settings.delay, 10);
+    if (isNaN(delay) || delay < 0) {
+      delay = 0;
+    }
+    delay = 1000 * ((delay - jitter) + (2 * jitter * Math.random()));
+
+    if (settings.list === 'blacklist') {
+      blacklist = settings.blacklist.toLowerCase().split(/\s+/);
+    }
+    else {
+      whitelist = settings.whitelist.toLowerCase().split(/\s+/);
+    }
+
+    event.target.page.dispatchMessage('receiveSettings', {
+      'blacklist': blacklist,
+      'delay': delay,
+      'timer': settings.timer,
+      'whitelist': whitelist
+    });
+  }
 }());
--- delay.safariextension/start.js
+++ delay.safariextension/start.js
@@ -1,51 +1,66 @@
 (function () {
   "use strict";
-  var active = true, block, elapsed = 0, intervalID, settings, tick = 1000;
+  var active = true;
 
   if (window === window.top) {
-    safari.self.tab.dispatchMessage('getSettings');
-    window.onblur = function () { active = false; };
-    window.onfocus = function () { active = true; };
+    safari.self.tab.dispatchMessage('requestSettings');
+    window.onblur = function () {
+      active = false;
+    };
+    window.onfocus = function () {
+      active = true;
+    };
   }
 
   safari.self.addEventListener('message', function (event) {
-    if (event.name === 'settings') {
-      settings = event.message;
-
-      if (settings.list === 'whitelist') {
-        block = settings.whitelist.indexOf(window.location.hostname) === -1;
-      }
-      else {
-        block = settings.blacklist.indexOf(window.location.hostname) !== -1;
-      }
-
-      if (block) {
-        if (settings.timer) {
-          document.documentElement.setAttribute('delay',
-          Math.round(settings.delay / 1000));
-        }
-        else {
-          document.documentElement.setAttribute('delay', '');
-        }
-
-        intervalID = window.setInterval(function () {
-          if (active) {
-            elapsed += tick;
-
-            if (settings.timer) {
-              document.documentElement.setAttribute('delay',
-              Math.round((settings.delay - elapsed) / 1000));
-            }
-            else {
-              document.documentElement.setAttribute('delay', '');
-            }
-          }
-          if (settings.delay - elapsed < 500) {
-            document.documentElement.removeAttribute('delay');
-            window.clearInterval(intervalID);
-          }
-        }, tick);
-      }
+    if (event.name === 'receiveSettings') {
+      return receiveSettings(event);
     }
   }, false);
+
+  function receiveSettings (event) {
+    var settings = event.message, delay;
+
+    if (settings.blacklist) {
+      delay = settings.blacklist.indexOf(window.location.hostname) !== -1;
+    }
+    else {
+      delay = settings.whitelist.indexOf(window.location.hostname) === -1;
+    }
+
+    if (delay) {
+      delayPage(settings.delay, settings.timer);
+    }
+  }
+
+  function delayPage (delay, timer) {
+    var intervalID, elapsed = 0;
+
+    if (timer) {
+      document.documentElement.setAttribute('delay',
+        Math.round(delay / 1000));
+    }
+    else {
+      document.documentElement.setAttribute('delay', '');
+    }
+
+    intervalID = window.setInterval(function () {
+      if (active) {
+        elapsed += 1000;
+      }
+
+      if (timer) {
+        document.documentElement.setAttribute('delay',
+          Math.round((delay - elapsed) / 1000));
+      }
+      else {
+        document.documentElement.setAttribute('delay', '');
+      }
+
+      if (delay - elapsed < 500) {
+        document.documentElement.removeAttribute('delay');
+        window.clearInterval(intervalID);
+      }
+    }, 1000);
+  }
 }());
